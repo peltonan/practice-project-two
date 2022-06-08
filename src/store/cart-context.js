15dd3e6c451c1524f6742d6c0dfcb7bd0884bd91
peltonan <peltonen.annette@gmail.com>
2022-06-08 16:09:59 +0300

    Fixed MealItemForm Error. useReduce & useContext still confusing. (Now at Section 11 - Part 147)

--- src/store/cart-context.js
+++ src/store/cart-context.js
@@ -3,8 +3,8 @@ import React from "react";
 const CartContext = React.createContext({
     items: [],
     totalAmount: 0,
-    addItem: () => { },
-    removeItem: (id) = {}
+    addItem: (item) => { },
+    removeItem: (id) => { },
 });
 
 export default CartContext;

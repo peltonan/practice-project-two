a0f2dd4026ba0b7c39e6e4fd8f0608f52f262822
peltonan <peltonen.annette@gmail.com>
2022-06-14 14:46:54 +0300

    Working with a database, backend. (Firebase) -- Section 17 // Part 219

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,11 +1,13 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 
 import Modal from "../UI/Modal";
 import CartItem from "./CartItem";
 import classes from './Cart.module.css'
 import CartContext from "../../store/cart-context";
+import Checkout from "./Checkout";
 
 const Cart = (props) => {
+    const [isCheckOut, setIsCheckOut] = useState(false);
     const cartCtx = useContext(CartContext);
 
     const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
@@ -17,6 +19,10 @@ const Cart = (props) => {
 
     const cartItemAddHandler = (item) => {
         cartCtx.addItem({ ...item, amount: 1 });
+    };
+
+    const orderHandler = () => {
+        setIsCheckOut(true);
     };
 
     const cartItems = (
@@ -32,6 +38,12 @@ const Cart = (props) => {
             ))}
         </ul>
     );
+
+    const modalActions = <div className={classes.actions}>
+        <button className={classes['button--alt']} onClick={props.onClose}>Close</button>
+        {hasItems && <button className={classes.button} onClick={orderHandler}>Order</button>}
+    </div>
+
     return (
         <Modal onClose={props.onClose}>
             {cartItems}
@@ -39,10 +51,9 @@ const Cart = (props) => {
                 <span>Total Amount</span>
                 <span>{totalAmount}</span>
             </div>
-            <div className={classes.actions}>
-                <button className={classes['button--alt']} onClick={props.onClose}>Close</button>
-                {hasItems && <button className={classes.button}>Order</button>}
-            </div>
+            {isCheckOut && <Checkout onCancel={props.onClose} />}
+            {!isCheckOut && modalActions}
+
         </Modal>
 
     );
